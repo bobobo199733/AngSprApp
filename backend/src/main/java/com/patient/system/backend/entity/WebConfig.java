4327423664c5527fcebbfd68c31abc29b0fd06fb
bobobo1997 <andrei.bogdan.marcel97@gmail.com>
2024-09-22 19:52:51 +0300

    Test

--- backend/src/main/java/com/patient/system/backend/entity/WebConfig.java
+++ backend/src/main/java/com/patient/system/backend/entity/WebConfig.java
@@ -2,15 +2,12 @@ package com.patient.system.backend.entity;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
-            .allowedOrigins("*") // Allow requests from Angular
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed methods
-            .allowedHeaders("*") // Allow all headers
-            .allowCredentials(false); // Allow credentials if needed
+            .allowedOrigins("*"); // Allow all origins
     }
 }
